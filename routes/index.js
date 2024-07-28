9af135a8a42c40d096ec783c3b00b9c9b8f1b3f9
Rafal Czajkowski <rafalxczajkowski@hotmail.com>
2024-07-28 17:29:51 +0200

    console.log remove

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', async (req, res) => {
+  console.log(req.get('host'))
+  console.log(req.originalUrl)
   res.render('index')
 })
 
